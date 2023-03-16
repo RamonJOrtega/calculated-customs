220c694468609a1b0fad934b6558ff6c85999e8e
ramon6767 <ramon.justis.ortega@gmail.com>
2023-03-16 16:10:38 -0700

    fix learn and about images

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -9,80 +9,91 @@ const inter = Inter({ subsets: ['latin'] })
 export default function LearnPage() {
     return(
         <div>
-            <div className={styles.overlayContainer}>
+            <div className={styles.pageImage}>
                 <Image 
                     src={pagePicture}
                     alt="Rohana Wheel"
                     placeholder="blur"
+                    fill={true}
+                    objectFit="cover"
                 />
+         
             </div> 
 
             <div className={styles.center}>
-                <div className={styles.pushLeft}>
+                <div className={styles.pageTextLeft}>
                     <div className={inter.className}>
                         <br></br>
                         <h1 >CALCULATING WHEEL AND TIRE PERFORMANCE</h1>
                         <br></br>
                     
-                        <h3>Why Trust Our Calculators? </h3>
-                        <p className={styles.card}> 
-                            We 3D-scanned real-world wheels and tires to calibrate our calculator parameters. <br></br>
+                        <h3> How Accurate Are Our Calculators? </h3>
+                        <br></br>
+                        <p > 
+                            We used 3D-scans of real-world wheels and tires to calibrate our calculators.
+                            Zero-to-sixty time calculations are currently accurate within 0.4 seconds.
                             <br></br>
                         </p>
-                        <h3>What Physics Used?</h3>
-                        <p className={styles.card}>
-                            Wheel torque rotates the tire and produces an opposing torque caused by the friction on the ground. <br></br> 
-                            The friction force is what actually moves the wheels and and the vehicle forward. <br></br>
-                            We sum all forces and put them into a torque sum equation and make  4 logical assumptions: <br></br>
+                        <br></br>
+                        <h3>How Are Our Calculators Modeled?</h3>
+                        <p >
+                            <br></br>
+                            Wheel torque rotates a car's tire and produces an opposing torque caused by the friction on the ground. 
+                            The friction force is what actually moves the wheels and and the vehicle forward. 
+                            We sum all forces and torques and make 4 logical assumptions: <br></br>
                         </p>
+                        <br></br>
                         <ol>
                             <li>
-                                <p className={styles.card}> 
+                                <p > 
                                     <strong>Tires do not slip. </strong> <br></br>
                                     This is true for those that achieve the fastest 0-60 times. 
-                                    Fast cars push the tires just up to the point of slipping, but not beyond that. <br></br>
+                                    The tires are pushed just up to the point of slipping. <br></br> <br></br>
                                 </p>
                             </li>
                             <li>
-                                <p className={styles.card}> 
+                                <p > 
                                     <strong>Wind resistance changes are negligible. </strong> <br></br>
-                                    This true for minimal velocity changes between 2 wheel/tire setups. <br></br>
+                                    This true for minimal velocity changes between 2 wheel/tire setups. <br></br><br></br>
                                 </p>
                             </li>
                             <li>
-                                <p className={styles.card}> 
+                                <p > 
                                     <strong>All 4 wheels and tires are the same size.</strong> <br></br>
                                     Most vehicles are manufactured this way. 
-                                    If you want staggered calculator functionality in the future. Let us know! <br></br>
+                                    If you want staggered calculator functionality in the future. Let us know! <br></br><br></br>
                                 </p>
                             </li>
                             <li>
-                                <p className={styles.card}> 
+                                <p > 
                                     <strong>Average torque, RPM, and acceleration are similar between 2 wheel/tire setups.</strong> <br></br>
                                     This is true for small changes (less than 1 second 0-60 time) when the RPM hasn’t had much time to change between 2 setups. <br></br>
                                 </p>
                             </li>
                         </ol> <br></br>
                         <h3>The Downsides of Rotating Mass </h3> 
-                        <p className={styles.card}>
-                            It’s true what gearheads say on the forums: rotating weight is worse than stationary weight. Why is that?  <br></br>
+                        <p >
+                        <br></br>
+                            It’s true what gearheads say on the forums: rotating weight is worse than stationary weight. Why is that?  <br></br><br></br>
                         </p>
-                        <p className={styles.card}>
-                            Force = Mass × Acceleration (F=ma), but cars supply limited force. <br></br>
-                            So adding mass reduces acceleration.  <br></br> 
+                        <p >
+                            Force = Mass × Acceleration (F=ma)  <br></br>
+                            Cars supply limited force, so adding mass reduces acceleration.  <br></br> <br></br>
                         </p>
-                        <p className={styles.card}>
-                            Torque = rotational inertia × angular acceleration (T=Iα), but cars supply limited torque. <br></br>
-                            So adding rotational inertia reduces angular acceleration. <br></br> 
+                        <p >
+                            Torque = rotational inertia × angular acceleration (T=Iα) <br></br>
+                            Cars supply limited torque, so adding rotational inertia reduces angular acceleration. <br></br> <br></br>
                         </p>
-                        <p className={styles.card}>
-                            However, <br></br>
+                        <p >
+                          
                             Rotational Inertia= Mass × Rotation Radius² (I=mr²). <br></br>
-                            Rotational Inertia increases directly with with mass (m) but EXPONENTIALLY with the distance (r) of every rotating component point from center.<br></br> 
-                            This is why adding rotating weight is miuch worse than adding stationary weight to a vehicle. <br></br> 
+                            Rotational Inertia increases directly with with mass (m) but EXPONENTIALLY with the distance (r) of every rotating component point from center.
+                            This is why adding rotating weight is much worse than adding stationary weight to a vehicle. <br></br> 
                             </p> 
-                            <p className={styles.card}>
-                            By the way, Rotational Inertia, Moment of Inertia, Rotating Mass, Rotating Weight are all the terms for theh same thing.
+                            <p >
+                                <br></br>
+                            By the way, Rotational Inertia, Moment of Inertia, Rotating Mass, Rotating Weight are all the terms for the same thing.
+                            <br></br>
                             </p>
                     </div>
                  </div>
