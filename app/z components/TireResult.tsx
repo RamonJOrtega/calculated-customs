646f4ec5c157c5847824ca352b5a84be17c41fd9
ramon6767 <ramon.justis.ortega@gmail.com>
2023-03-20 17:11:17 -0700

    move all constants from TireResult.tsx and WheelResult.tsx componenents to new file called constants.tsx git

--- app/z components/TireResult.tsx
+++ app/z components/TireResult.tsx
@@ -3,6 +3,7 @@ import styles from '../page.module.css'
 import Link from  'next/link';
 import '../globals.css'
 import Image from 'next/image';
+import * as C from '../constants'
 
 interface TireResultProps {
     wheelDiameter: string;
@@ -16,10 +17,7 @@ interface TireResultProps {
     
 }
  const TireResult: React.FC<TireResultProps> = (props) => {
-    const METER_PER_INCH = 0.0254
-    const KILOGRAM_PER_POUND = 0.453592
-    const SIDEWALL_MASS_PERCENTAGE = 0.25
-    const TREAD_MASS_PERCENTAGE = 0.75
+    
     
     let tireResult: string = "";
     if (props.isStandardNotation ) {calcTireDiaFromStandardNotation()}
@@ -30,11 +28,11 @@ interface TireResultProps {
         props.setTireDiameter(parseFloat(props.wheelDiameter) + 2*sideWallHeight_in)
       }
     function calcTireDiaFromTireWheelDiaNotation():void{
-        const tireRad_m = parseFloat(props.tireDiameter) * METER_PER_INCH/2                          // console.log('tire radius in meters: ' + tireRad_m)
-        const tireMass_kg = parseFloat(props.tireWeight) * KILOGRAM_PER_POUND                         // console.log('tire mass in kilograms: ' + tireMass_kg)
-        const treadInertia = TREAD_MASS_PERCENTAGE* tireMass_kg * tireRad_m * tireRad_m                     // console.log('treadInertia in kg*m^2: ' + treadInertia)
-        const wheelRad_m = parseFloat(props.wheelDiameter) * METER_PER_INCH / 2
-        const sidewallInertia = SIDEWALL_MASS_PERCENTAGE * (1/2) * tireMass_kg * (tireRad_m * tireRad_m + wheelRad_m * wheelRad_m)// console.log('sidewallInertia in kg*m^2: ' + sidewallInertia)
+        const tireRad_m = parseFloat(props.tireDiameter) * C.METER_PER_INCH/2                          // console.log('tire radius in meters: ' + tireRad_m)
+        const tireMass_kg = parseFloat(props.tireWeight) * C.KILOGRAM_PER_POUND                         // console.log('tire mass in kilograms: ' + tireMass_kg)
+        const treadInertia = C.TREAD_MASS_PERCENTAGE* tireMass_kg * tireRad_m * tireRad_m                     // console.log('treadInertia in kg*m^2: ' + treadInertia)
+        const wheelRad_m = parseFloat(props.wheelDiameter) * C.METER_PER_INCH / 2
+        const sidewallInertia = C.SIDEWALL_MASS_PERCENTAGE * (1/2) * tireMass_kg * (tireRad_m * tireRad_m + wheelRad_m * wheelRad_m)// console.log('sidewallInertia in kg*m^2: ' + sidewallInertia)
         tireResult = (treadInertia + sidewallInertia).toFixed(1)
         props.setTireInertiaResult(tireResult)
     }
--- app/z components/WheelResult.tsx
+++ app/z components/WheelResult.tsx
@@ -3,6 +3,7 @@ import styles from '../page.module.css'
 import Link from  'next/link';
 import '../globals.css'
 import Image from 'next/image';
+import * as C from '../constants'
 
 
 interface WheelResultProps {
@@ -11,18 +12,13 @@ interface WheelResultProps {
     setWheelInertiaResult: any;
 }
  const WheelResult: React.FC<WheelResultProps> = (props) => {
-    const METER_PER_INCH = 0.0254
-    const KILOGRAM_PER_POUND = 0.453592
-    const HUB_RADIUS_METERS =  0.165
-    const SPOKE_MASS_PERCENTAGE = 0.25
-    const HUB_MASS_PERCENTAGE = 0.20
-    const RIM_MASS_PERCENTAGE = 0.55
+   
 
-    const wheelRad_m = parseFloat(props.wheelDiameter) * METER_PER_INCH / 2                                  //  console.log('wheel radius in meters: ' + wheelRad_m)
-    const wheelMass_kg = parseFloat(props.wheelWeight) * KILOGRAM_PER_POUND                                  //console.log('tire mass in kilograms: ' + wheelMass_kg)
-    const spokeInertia = SPOKE_MASS_PERCENTAGE* (1/2) * wheelMass_kg * (wheelRad_m*wheelRad_m +HUB_RADIUS_METERS *HUB_RADIUS_METERS)  //console.log("spokeInertia is " + spokeInertia)
-    const hubInertia = HUB_MASS_PERCENTAGE * (1/2) * wheelMass_kg *HUB_RADIUS_METERS *HUB_RADIUS_METERS                              //console.log("hub inertia is " + hubInertia)
-    const rimInertia = RIM_MASS_PERCENTAGE * wheelMass_kg * wheelRad_m * wheelRad_m                            //console.log("RIM inertia is " + rimInertia)
+    const wheelRad_m = parseFloat(props.wheelDiameter) * C.METER_PER_INCH / 2                                  //  console.log('wheel radius in meters: ' + wheelRad_m)
+    const wheelMass_kg = parseFloat(props.wheelWeight) * C. KILOGRAM_PER_POUND                                  //console.log('tire mass in kilograms: ' + wheelMass_kg)
+    const spokeInertia = C. SPOKE_MASS_PERCENTAGE* (1/2) * wheelMass_kg * (wheelRad_m*wheelRad_m +C.HUB_RADIUS_METERS *C.HUB_RADIUS_METERS)  //console.log("spokeInertia is " + spokeInertia)
+    const hubInertia = C. HUB_MASS_PERCENTAGE * (1/2) * wheelMass_kg *C. HUB_RADIUS_METERS *C.HUB_RADIUS_METERS                              //console.log("hub inertia is " + hubInertia)
+    const rimInertia = C.RIM_MASS_PERCENTAGE * wheelMass_kg * wheelRad_m * wheelRad_m                            //console.log("RIM inertia is " + rimInertia)
     const wheelInertia = (rimInertia + spokeInertia + hubInertia).toFixed(1)
     props.setWheelInertiaResult(wheelInertia)
   
