18d76c0ad95dbc055bf22234dc7eeaa41fa6b45f
ramon6767 <ramon.justis.ortega@gmail.com>
2023-04-14 11:58:25 -0700

    add tip text to title fields respectively in MainCalculator.tsx by hooking up CalcInput components

--- app/z components/MainCalculator.tsx
+++ app/z components/MainCalculator.tsx
@@ -3,7 +3,6 @@
 import TireResult from "./TireResult"
 import TotalResult from "./TotalResult"
 import WheelResult from "./WheelResult"
-import FieldTitle from "./FieldTitle"
 import React, { SetStateAction } from "react"
 import FieldInput  from "./FieldInput"  ;
 import StandarNotationCheckBox from "./StandardNotationCheckBox";
@@ -13,6 +12,7 @@ import { Inter } from 'next/font/google'
 import KnownRadioButton from "./KnownRadioButton"
 import { Main } from "next/document"
 import CalcInput from "./CalcInput"
+import CalcTitle from "./CalcTitle"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -61,59 +61,59 @@ const MainCalculator = () => {
                 },
                 {
                     title1: "Tire Diameter",    title2: "Standard Notation",title3: "Wheel Diameter",
-                    value1: tireDia1,  value2: isStandardNotation1,value3: wheelDia1,
+                    value1: tireDia1,           value2: isStandardNotation1,value3: wheelDia1,
                     unit1:  "inch",             unit2:   "",                unit3:  "inch",
                     setValue1: setTireDia1,     setValue2: setIsStandardNotation1, setValue3: setWheelDia1
                 },
                 {
-                    title1: "Tire Width",    title2: "Aspect Ratio",        title3: "Wheel Diameter",
-                    value1: tireWd1,        value2: tireAsp1,               value3: wheelDia1,
-                    unit1:  "/",             unit2:"R",              unit3:  "inch",
+                    title1: "Tire Width",       title2: "Aspect Ratio",     title3: "Wheel Diameter",
+                    value1: tireWd1,            value2: tireAsp1,           value3: wheelDia1,
+                    unit1:  "/",                unit2:"R",                  unit3:  "inch",
                     setValue1: setTireWd1,      setValue2: setTireAsp1, setValue3: setWheelDia1
                 },
                 {
                     title1: "Tire Weight",      title2: "",                 title3: "Wheel Weight",
                     value1: tireWt1 ,           value2: "",                 value3: wheelWt1,
                     unit1: "pound",             unit2: "",                  unit3: "pound",
-                    setValue1: setTireWt1, setValue2: null,                setValue3: setWheelWt1
+                    setValue1: setTireWt1,      setValue2: null,            setValue3: setWheelWt1
                 },
                 {
-                    title1: "Tire Inertia",         title2: "Total Inertia",        title3: "Wheel Inertia",
-                    value1: tireInertia1,  value2: totalInertia1, value3: wheelInertia1,
-                    unit1: "kgm2",              unit2: "kgm2",              unit3: "kgm2",
+                    title1: "Tire Inertia",         title2: "Total Inertia",    title3: "Wheel Inertia",
+                    value1: tireInertia1,           value2: totalInertia1,      value3: wheelInertia1,
+                    unit1: "kgm2",                  unit2: "kgm2",              unit3: "kgm2",
                     setValue1: setTireInertia1, setValue2: setTotalInertia1, setValue3: setWheelInertia1
 
                 },
                 {
-                    title1: "Vehecle Weight Known", title2: "", title3: "Current 0-60 Time",
-                    value1: vehicleWt,     value2: "", value3: currentZeroSixtyTime,
-                    unit1: "pound",                 unit2: "",  unit3: "second",
-                    setValue1: setVehicleWt, setValue2: null, setValue3: setCurrentZeroSixtyTime
+                    title1: "Vehecle Weight Known", title2: "",         title3: "Current 0-60 Time",
+                    value1: vehicleWt,              value2: "",         value3: currentZeroSixtyTime,
+                    unit1: "pound",                 unit2: "",          unit3: "second",
+                    setValue1: setVehicleWt,        setValue2: null,    setValue3: setCurrentZeroSixtyTime
                 },
                 {
                     title1: "Setup Combo 2", title2: "Calculate Rotational Inertia and Compare 0-60", title3: "",
-                    value1: "",             value2:  isComparisonEnabled,                                        value3: "",
+                    value1: "",             value2:  isComparisonEnabled,                            value3: "",
                     unit1:  "",             unit2:   "",                                            unit3:  "",
                     setValue1: null, setValue2: null, setValue3: null
                 },
         
                 {
                     title1: "Tire Diameter",    title2: "Standard Notation",title3: "Wheel Diameter",
-                    value1: tireDia2,  value2: isStandardNotation2,value3: wheelDia2,
+                    value1: tireDia2,           value2: isStandardNotation2,value3: wheelDia2,
                     unit1:  "inch",             unit2:   "",                unit3:  "inch",
                     setValue1: setTireDia2,     setValue2: setIsStandardNotation2, setValue3: setWheelDia2
                 },
                 {
                     title1: "Tire Width",    title2: "Aspect Ratio",        title3: "Wheel Diameter",
-                    value1: tireWd2,        value2: tireAsp2,               value3: wheelDia2,
-                    unit1:  "/",             unit2:"R",              unit3:  "inch",
+                    value1: tireWd2,            value2: tireAsp2,               value3: wheelDia2,
+                    unit1:  "/",             unit2:"R",                     unit3:  "inch",
                     setValue1: setTireWd2,      setValue2: setTireAsp2, setValue3: setWheelDia2
                 },
                 {
                     title1: "Tire Weight",      title2: "",                 title3: "Wheel Weight",
                     value1: tireWt2 ,           value2: "",                 value3: wheelWt2,
                     unit1: "pound",             unit2: "",                  unit3: "pound",
-                    setValue1: setTireWt2, setValue2: null,                setValue3: setWheelWt2
+                    setValue1: setTireWt2, setValue2: null,                 setValue3: setWheelWt2
                 },
                 {
                     title1: "Tire Inertia",         title2: "Total Inertia",        title3: "Wheel Inertia",
@@ -129,9 +129,9 @@ const MainCalculator = () => {
             {calculatorLayout.map((row, index)=>(
                 <tbody key={"row" + index}> 
                     <tr>
-                        <td colSpan={2}><FieldTitle title={row.title1}/> </td>
-                        <td colSpan={2}><FieldTitle title={row.title2}/> </td>
-                        <td colSpan={2}><FieldTitle title={row.title3}/> </td>                  
+                        <td colSpan={2}><CalcTitle title={row.title1}/> </td>
+                        <td colSpan={2}><CalcTitle title={row.title2}/> </td>
+                        <td colSpan={2}><CalcTitle title={row.title3}/> </td>                  
                     </tr>
                     <tr>
                         <td><CalcInput value={row.value1} setValue={row.setValue1} title = {row.title1}/> </td>
