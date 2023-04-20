8de371c5c2225506a45a520a792f92b92bec114e
ramon6767 <ramon.justis.ortega@gmail.com>
2023-04-20 16:40:10 -0700

    refactor with fewer lines of code CalcTitle.tsx

--- app/z components/CalcTitle.tsx
+++ app/z components/CalcTitle.tsx
@@ -14,31 +14,16 @@ export default function FieldTitle(props: any) {
     const tireTipText = "Optionally use standard notation to set tire diameter"
     const intertiaTipText = "Also called moment of inertia or rotating mass. Keep it low to go faster!"
 
-    return(
-        props.isVisible ? (
-            <div className={styles.fieldTitle}>  
-
-            {props.title} 
-
-            {(props.title === "Standard Notation") && (
-                <span className={styles.tooltip}>&#x24D8;
-                    <span className={styles.tooltiptext}>
-                        {tireTipText}
-                    </span>
-                </span>
-            )}
-
-            {(props.title === "Total Inertia") && (
-                <span className={styles.tooltip}>&#x24D8;
-                    <span className={styles.tooltiptext}>
-                        {intertiaTipText}
-                    </span>
-                </span>
-            )}
-
+    return props.isVisible ? (
+        <div className={styles.fieldTitle}>
+          {props.title}
+          {(props.title === "Standard Notation" || props.title === "Total Inertia") && (
+            <span className={styles.tooltip}>&#x24D8;
+              <span className={styles.tooltiptext}>
+                {props.title === "Standard Notation" ? tireTipText : intertiaTipText}
+              </span>
+            </span>
+          )}
         </div>
-
-        ) : null
-
-    )
-} 
+      ) : null;
+    }
