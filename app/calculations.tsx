4e20b00933214ccfc93c028c441405c7d9a2bfbd
ramon6767 <ramon.justis.ortega@gmail.com>
2023-05-05 15:14:49 -0700

    modify all  ternary empty string checkers in calculations.tsx to be a decrostructing short circuit assignment

--- app/calculations.tsx
+++ app/calculations.tsx
@@ -1,8 +1,7 @@
 import * as C from "./constants"
 
 export const calculateWheelInertia = (wheelDia: string, wheelWt: string): string => {
-    wheelDia = (wheelDia ==="") ? "0" : wheelDia
-    wheelWt = (wheelWt ==="") ? "0" : wheelWt
+    [wheelDia, wheelWt] = [wheelDia || "0", wheelWt || "0"]
     const wheelRad_m = parseFloat(wheelDia) * C.METER_PER_INCH / 2                                  //  console.log('wheel radius in meters: ' + wheelRad_m)
     const wheelMass_kg = parseFloat(wheelWt) * C.KILOGRAM_PER_POUND                                  //console.log('tire mass in kilograms: ' + wheelMass_kg)
     const spokeInertia = C.SPOKE_MASS_PERCENTAGE* (1/2) * wheelMass_kg * (wheelRad_m*wheelRad_m +C.HUB_RADIUS_METERS *C.HUB_RADIUS_METERS)  //console.log("spokeInertia is " + spokeInertia)
@@ -13,6 +12,7 @@ export const calculateWheelInertia = (wheelDia: string, wheelWt: string): string
 }
 
 export const calculateTotalInertia = (tireInertia: string, wheelInertia: string): string => {
+    [tireInertia, wheelInertia] = [tireInertia || "0", wheelInertia || "0"]
     tireInertia = (tireInertia ==="") ? "0" : tireInertia
     wheelInertia = (wheelInertia ==="") ? "0" : wheelInertia
     tireInertia = (tireInertia ==="") ? "0" : tireInertia
@@ -22,9 +22,7 @@ export const calculateTotalInertia = (tireInertia: string, wheelInertia: string)
 }
 
 export const calculateTireInertia = (tireDia: string, tireWt: string, wheelDia: string): string => {
-    tireDia = (tireDia ==="") ? "0" : tireDia
-    tireWt = (tireWt ==="") ? "0" : tireWt
-    wheelDia = (wheelDia ==="") ? "0" : wheelDia
+    [tireDia, tireWt, wheelDia] = [tireDia || "0", tireWt || "0", wheelDia || "0"]
     const tireRad_m = parseFloat(tireDia) * C.METER_PER_INCH/2                          // console.log('tire radius in meters: ' + tireRad_m)
     const tireMass_kg = parseFloat(tireWt) * C.KILOGRAM_PER_POUND                        // console.log('tire mass in kilograms: ' + tireMass_kg)
     const treadInertia = C.TREAD_MASS_PERCENTAGE* tireMass_kg * tireRad_m * tireRad_m                     // console.log('treadInertia in kg*m^2: ' + treadInertia)
@@ -36,7 +34,6 @@ export const calculateTireInertia = (tireDia: string, tireWt: string, wheelDia: 
 
 export const calculateTireDiameterFromStandardNotation = (aspectRatio: string, tireWidth: string, wheelDiameter: string): string => {
     [aspectRatio, tireWidth, wheelDiameter] = [aspectRatio || "0", tireWidth || "0", wheelDiameter || "0"];
-
     const sideWallHeight_in = parseFloat(aspectRatio)/100 * parseFloat(tireWidth)/25.4
     const tireDiameter = (parseFloat(wheelDiameter) + 2*sideWallHeight_in)
     return tireDiameter.toString()
