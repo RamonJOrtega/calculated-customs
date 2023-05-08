269dc9a3dfc631e51e3e1accbbb740693d680d7a
ramon6767 <ramon.justis.ortega@gmail.com>
2023-05-07 18:01:09 -0700

    initialize setup values in MainCalculator.tsx and fix tire inertia bug in caclulations.tsx

--- app/calculations.tsx
+++ app/calculations.tsx
@@ -19,7 +19,9 @@ export const calcTotalInertia = (tireInertia: string, wheelInertia: string): str
 
 export const calcTireInertia = (tireDia: string, tireWt: string, wheelDia: string, isStdNtn: boolean, aspRat: string, tireWd: string): string => {
     [tireDia, tireWt, wheelDia, aspRat, tireWd] = [tireDia || "0", tireWt || "0", wheelDia || "0", aspRat || "0", tireWd || "0"]
-    if (!isStdNtn) {
+    if (isStdNtn) {
+        tireDia = calcTireDiaFromStdNotation(aspRat, tireWd, wheelDia)
+    }
         const tireRad_m = parseFloat(tireDia) * C.METER_PER_INCH/2                          // console.log('tire radius in meters: ' + tireRad_m)
         const tireMass_kg = parseFloat(tireWt) * C.KILOGRAM_PER_POUND                        // console.log('tire mass in kilograms: ' + tireMass_kg)
         const treadInertia = C.TREAD_MASS_PERCENTAGE* tireMass_kg * tireRad_m * tireRad_m                     // console.log('treadInertia in kg*m^2: ' + treadInertia)
@@ -27,15 +29,14 @@ export const calcTireInertia = (tireDia: string, tireWt: string, wheelDia: strin
         const sidewallInertia = C.SIDEWALL_MASS_PERCENTAGE * (1/2) * tireMass_kg * (tireRad_m * tireRad_m + wheelRad_m * wheelRad_m)// console.log('sidewallInertia in kg*m^2: ' + sidewallInertia)
         const tireInertia = (treadInertia + sidewallInertia).toFixed(1)
         return tireInertia
-    } else {
-        return calcTireDiaFromStdNotation(aspRat, tireWd, wheelDia)
-    }
-
+    
 }
 
 export const calcTireDiaFromStdNotation = (aspRat: string, tireWd: string, wheelDia: string): string => {
     if (aspRat === "0" || tireWd === "0" || wheelDia === "0") {return "0"}
     const sideWallHeight_in =  parseFloat(aspRat) / 100 * parseFloat(tireWd) / 25.4;
     const tireDiameter =  (parseFloat(wheelDia) + 2 * sideWallHeight_in).toFixed(1)
+    console.log(tireDiameter)
     return tireDiameter;
 }
+
