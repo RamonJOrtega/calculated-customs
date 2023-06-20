2bfc09c1b8078f4aa47af4029592fc147648ed16
RamonJOrtega <ramon.justis.ortega@gmail.com>
2023-06-20 16:54:42 -0700

    install tailwind and add to globals.css

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -2,6 +2,8 @@
 import { Inter } from 'next/font/google'
 import dynamic from 'next/dynamic'
 import Link from 'next/link'
+import "../globals.css"
+
 
 import TestCalculator from '../z components/TestCalculator'
 import NavigationBar from '../z components/NavigationBar'
@@ -14,6 +16,7 @@ export default function Home() {
 
   return (
     <> 
+        <h1 className="text-4xl font-bold underline"> Hello world</h1>
         <NavigationBar />
         <TestCalculator />
 
