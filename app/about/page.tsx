19be154dc87f8b7131cb5965946e6149e924e27f
RamonJOrtega <ramon.justis.ortega@gmail.com>
2023-06-28 18:27:00 -0700

    style and save about us page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -17,95 +17,75 @@ export default function LearnPage() {
                 />
             </div> 
 
-            <div >
-                <div >
+            <section >
                     <div >
+                    <br></br>
+                    <h1 >ABOUT US</h1>
+                    
+                    <p  > 
+                        <a href="https://www.tkqlhce.com/click-100641204-10377976">Find your wheels</a>.&#160;
+                        <a href="https://www.kqzyfj.com/click-100641204-10377851">Find your tires</a>.&#160;
+                        <a href="/">Compare</a>.
+                    </p>
+                    <br></br>
+                    
+                    <p >
+                        You’ll KNOW which combo is faster BEFORE you spend thousands of dollars on your whip’s new kicks. <br></br>
+                    </p>
+                    <br></br>
+                    <p >
+                        Support us by clicking affiliate links and purchasing wheels or tires through &#160;
+                        <a href="https://www.jdoqocy.com/click-100641204-10377982">Tire Rack</a>&#160;/&#160;<a href="https://www.tkqlhce.com/click-100641204-13382742">Discount Tire</a>,&#160; 
+                        <a href="https://www.dpbolvw.net/click-100641204-15280196">Hoonigan</a>,&#160; and &#160; 
+                        <a href="https://www.dpbolvw.net/click-100641204-15357094">Throttle</a>.&#160;  
                         <br></br>
-                        <h1 >ABOUT US</h1>
-                        
-                       
-                        <p  > 
-                                     Find your &#160;<span><a href="https://www.tkqlhce.com/click-100641204-10377976">wheels.</a> &#160; </span>
-                                    Find your &#160;<span><a href="https://www.kqzyfj.com/click-100641204-10377851">tires</a>. &#160; </span>
-                                    <span><a href="/"> Compare.</a></span>
-                        </p>
+                        The commissions generated will be used to add more calculators and products to our website. 
                         <br></br>
-                       
-                        <p >
-                          
-                            You’ll KNOW which combo is faster BEFORE you spend thousands of dollars on your whip’s new kicks. <br></br>
+                        <br></br>                        
+                    </p>
+                </div>
 
-                        </p>
-                        <br></br>
-                        <p >
-                       
-                            Support us by clicking affiliate links and purchasing wheels or tires through &#160;
-                            <span><a href="https://www.jdoqocy.com/click-100641204-10377982">Tire Rack</a></span>&#160;/&#160;<span><a href="https://www.tkqlhce.com/click-100641204-13382742">Discount Tire</a>,&#160;</span> 
-                            <span><a href="https://www.dpbolvw.net/click-100641204-15280196">Hoonigan</a>,&#160;</span> and &#160; 
-                            <span><a href="https://www.dpbolvw.net/click-100641204-15357094">Throttle</a>.&#160;</span>  
-                            <br></br>
-                            The commissions generated will be used to add more calculators and products to our website. 
-                            <br></br>
-                            <br></br>                        
-                        </p>
-                    </div>
-                 </div>
-                 <div >
-                <div >
+                <div className="flex flex-row flex-wrap justify-between">
                     <a  href="https://www.tkqlhce.com/click-100641204-10376887" target="_top">
-                        <span>
-                            <img src="https://www.ftjcfx.com/image-100641204-10376887" width="88" height="31" alt="Tirerack.com- Revolutionizing Tire Buying" />
-                        </span>
+                        <img src="https://www.ftjcfx.com/image-100641204-10376887" width="88" height="31" alt="Tirerack.com- Revolutionizing Tire Buying" />
                     </a> 
-                </div>
 
-                <div >
                     <a href="https://www.tkqlhce.com/click-100641204-13375597" target="_top">
-                        <span>
-                            <img src="https://www.awltovhc.com/image-100641204-13375597"  height="31" alt="" />
-                        </span>
+                        <img src="https://www.awltovhc.com/image-100641204-13375597" width="88"  height="31" alt="" />
                     </a> 
-                </div>
 
-                <div >
                     <a href="https://www.anrdoezrs.net/click-100641204-15280193" target="_top">
-                        <span>
-                            <img src="https://www.awltovhc.com/image-100641204-15280193" width="88" height="31" alt="Hoonigan" />
-                        </span>
+                        <img src="https://www.awltovhc.com/image-100641204-15280193" width="88" height="31" alt="Hoonigan" />
                     </a> 
-                </div>
 
-                <div >
                     <a href="https://www.kqzyfj.com/click-100641204-15280215" target="_top">
-                        <span>
-                            <img src="https://www.tqlkg.com/image-100641204-15280215" width="88" height="31" alt="Throtl" />
-                        </span>
+                        <img src="https://www.tqlkg.com/image-100641204-15280215" width="88" height="31" alt="Throtl" />
                     </a> 
                 </div>
-            </div>
+            </section>
+
             <br></br> <br></br>
-            <div >
+                <section className='flex items-center flex-col'>
+                    <div className="overflow-hidden rounded-full">
                         <Image
                         src={president}
                         alt="President Image"
                         height={150}
                         placeholder="blur"
                         />
-                </div>
+                    </div>
                 
-                <div >
-                        <br></br>
-                        <h5>Ramon Ortega</h5>
-                        <br></br>
-                        <p >
-                            The president of Calculated Customs. <br></br>
-                            M.S. in Mechanical Engineering, 8-year Gas Turbine Engineer, <br></br>
-                            and software developer when not obsessing over wheels.
-                        </p>
-                        <br></br>
-                </div>       
+                    <br></br>
+                    <h5>Ramon Ortega</h5>
+                    <br></br>
+                    <p className="text-center">
+                        The president of Calculated Customs. <br></br>
+                        M.S. in Mechanical Engineering, 8-year Gas Turbine Engineer, <br></br>
+                        and software developer when not obsessing over wheels.
+                    </p>
+                    <br></br>
+                </section>
             </div>   
-        </div>
          
     )
 }
--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -14,89 +14,83 @@ export default function LearnPage() {
                     fill={true}
                     objectFit="cover"
                 />
-         
+        
             </div> 
 
-            <div >
-                <div >
-                    <div >
-                        <br></br>
-                        <h1  >CALCULATING WHEEL AND TIRE PERFORMANCE</h1>
-                        <br></br>
+            <br></br>
+            <h1  >CALCULATING WHEEL AND TIRE PERFORMANCE</h1>
+            <br></br>
 
-                       <section>
-                        <h3> How Accurate Are Our Calculators? </h3>
-                            <br></br>
-                            <p > 
-                                We used 3D-scans of real-world wheels and tires to calibrate our calculators.
-                                Zero-to-sixty time calculations are currently accurate within 0.4 seconds.
-                                <br></br>
-                            </p>
-                            <br></br>
-                       </section>
-                        <h3>How Are Our Calculators Modeled?</h3>
-                        <p >
-                            <br></br>
-                            Wheel torque rotates a car's tire and produces an opposing torque caused by the friction on the ground. 
-                            The friction force is what actually moves the wheels and and the vehicle forward. 
-                            We sum all forces and torques and make 4 logical assumptions: <br></br>
-                        </p>
-                        <br></br>
-                        <ol>
-                            <li>
-                                <p > 
-                                    <strong>Tires do not slip. </strong> <br></br>
-                                    This is true for those that achieve the fastest 0-60 times. 
-                                    The tires are pushed just up to the point of slipping. <br></br> <br></br>
-                                </p>
-                            </li>
-                            <li>
-                                <p > 
-                                    <strong>Wind resistance changes are negligible. </strong> <br></br>
-                                    This true for minimal velocity changes between 2 wheel/tire setups. <br></br><br></br>
-                                </p>
-                            </li>
-                            <li>
-                                <p > 
-                                    <strong>All 4 wheels and tires are the same size.</strong> <br></br>
-                                    Most vehicles are manufactured this way, but staggered fitment calculators will be available in the future.
-                                    <br></br><br></br>
-                                </p>
-                            </li>
-                            <li>
-                                <p > 
-                                    <strong>Average torque, RPM, and acceleration are similar between 2 wheel/tire setups.</strong> <br></br>
-                                    This is true for small changes (less than 1 second 0-60 time) when the RPM hasn’t had much time to change between 2 setups. <br></br>
-                                </p>
-                            </li>
-                        </ol> <br></br>
-                        <h3>The Downsides of Rotating Mass </h3> 
-                        <p >
-                        <br></br>
-                            It’s true what gearheads say on the forums: rotating weight is worse than stationary weight. Why is that?  <br></br><br></br>
-                        </p>
-                        <p >
-                            Force = Mass × Acceleration (F=ma)  <br></br>
-                            Cars supply limited force, so adding mass reduces acceleration.  <br></br> <br></br>
-                        </p>
-                        <p >
-                            Torque = rotational inertia × angular acceleration (T=Iα) <br></br>
-                            Cars supply limited torque, so adding rotational inertia reduces angular acceleration. <br></br> <br></br>
-                        </p>
-                        <p >
-                          
-                            Rotational Inertia= Mass × Rotation Radius² (I=mr²). <br></br>
-                            Rotational Inertia increases directly with with mass (m) but EXPONENTIALLY with the distance (r) of every rotating component point from .
-                            This is why adding rotating weight is much worse than adding stationary weight to a vehicle. <br></br> 
-                            </p> 
-                            <p >
-                                <br></br>
-                            By the way, Rotational Inertia, Moment of Inertia, Rotating Mass, Rotating Weight are all the terms for the same thing.
-                            <br></br>
-                            </p>
-                    </div>
-                 </div>
-            </div>
+            <section>
+            <h3> How Accurate Are Our Calculators? </h3>
+                <br></br>
+                <p > 
+                    We used 3D-scans of real-world wheels and tires to calibrate our calculators.
+                    Zero-to-sixty time calculations are currently accurate within 0.4 seconds.
+                    <br></br>
+                </p>
+                <br></br>
+            </section>
+            <h3>How Are Our Calculators Modeled?</h3>
+            <p >
+                <br></br>
+                Wheel torque rotates a car's tire and produces an opposing torque caused by the friction on the ground. 
+                The friction force is what actually moves the wheels and and the vehicle forward. 
+                We sum all forces and torques and make 4 logical assumptions: <br></br>
+            </p>
+            <br></br>
+            <ol>
+                <li>
+                    <p > 
+                        <strong>Tires do not slip. </strong> <br></br>
+                        This is true for those that achieve the fastest 0-60 times. 
+                        The tires are pushed just up to the point of slipping. <br></br> <br></br>
+                    </p>
+                </li>
+                <li>
+                    <p > 
+                        <strong>Wind resistance changes are negligible. </strong> <br></br>
+                        This true for minimal velocity changes between 2 wheel/tire setups. <br></br><br></br>
+                    </p>
+                </li>
+                <li>
+                    <p > 
+                        <strong>All 4 wheels and tires are the same size.</strong> <br></br>
+                        Most vehicles are manufactured this way, but staggered fitment calculators will be available in the future.
+                        <br></br><br></br>
+                    </p>
+                </li>
+                <li>
+                    <p > 
+                        <strong>Average torque, RPM, and acceleration are similar between 2 wheel/tire setups.</strong> <br></br>
+                        This is true for small changes (less than 1 second 0-60 time) when the RPM hasn’t had much time to change between 2 setups. <br></br>
+                    </p>
+                </li>
+            </ol> <br></br>
+            <h3>The Downsides of Rotating Mass </h3> 
+            <p >
+            <br></br>
+                It’s true what gearheads say on the forums: rotating weight is worse than stationary weight. Why is that?  <br></br><br></br>
+            </p>
+            <p >
+                Force = Mass × Acceleration (F=ma)  <br></br>
+                Cars supply limited force, so adding mass reduces acceleration.  <br></br> <br></br>
+            </p>
+            <p >
+                Torque = rotational inertia × angular acceleration (T=Iα) <br></br>
+                Cars supply limited torque, so adding rotational inertia reduces angular acceleration. <br></br> <br></br>
+            </p>
+            <p >
+                
+                Rotational Inertia= Mass × Rotation Radius² (I=mr²). <br></br>
+                Rotational Inertia increases directly with with mass (m) but EXPONENTIALLY with the distance (r) of every rotating component point from .
+                This is why adding rotating weight is much worse than adding stationary weight to a vehicle. <br></br> 
+                </p> 
+                <p >
+                    <br></br>
+                By the way, Rotational Inertia, Moment of Inertia, Rotating Mass, Rotating Weight are all the terms for the same thing.
+                <br></br>
+                </p>
         </div>
          
     )
