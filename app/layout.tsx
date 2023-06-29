b3b5fb15c08c4d737e709ad993d4caf9ecc5cd54
RamonJOrtega <ramon.justis.ortega@gmail.com>
2023-06-28 17:16:22 -0700

    update global.css with input width of 98% percent to get table to grow and shrink correctly

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,7 @@ import './globals.css'
 
 import { Analytics } from '@vercel/analytics/react';
 
-
 const inter = Inter({ subsets: ['latin'] })
-
 
 export const metadata = {
   title: 'Calculated Customs',
