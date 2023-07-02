c277aa2ff27af18f11e5e0513ccfd770c2a52750
RamonJOrtega <ramon.justis.ortega@gmail.com>
2023-07-02 09:07:06 -0700

    add .circle class to golbals.css and update Navbar.tsx component to begin gover movement of logo

--- app/z components/NavBar.tsx
+++ app/z components/NavBar.tsx
@@ -48,75 +48,52 @@ import { SetStateAction, useState } from 'react';
 // }
 
 
-const NavBar: React.FC = () => {
-  const [hoveredLink, setHoveredLink] = useState('');
 
-  const handleLinkHover = (link: string) => {
-    setHoveredLink(link);
+const Navbar = () => {
+  const [circlePosition, setCirclePosition] = useState(0);
+
+  const handleLinkHover = (index) => {
+    setCirclePosition(index);
   };
 
   return (
     <>
-      <nav className="flex justify-between flex-wrap mb-8">
-        <Link href="/">
-          <div className="flex items-center">
-            <h3 className="flex items-center transition-transform duration-300">
+      <nav className='flex justify-between flex-wrap mb-8'>
+        <Link href='/'>
+          <div>
+            <h3 className='flex items-center'>
               CALCULATED CUSTOMS
-              <span
-                className={`ml-2 transform transition-transform duration-300 ${
-                  hoveredLink === '/' ? 'translate-x-1' : 'translate-x-0'
-                }`}
-              >
-                <Image src="/logo.svg" alt="calculated customs logo" width={25} height={25} priority />
-              </span>
             </h3>
           </div>
         </Link>
-        <Link href="/learn">
-          <div
-            onMouseEnter={() => handleLinkHover('/learn')}
-            onMouseLeave={() => handleLinkHover('')}
-            className={`transition-transform duration-300 ${
-              hoveredLink === '/learn' ? 'translate-x-1' : 'translate-x-0'
-            }`}
-          >
-            <h5>
-              Learn <span>-&gt;</span>
-            </h5>
+        <Link href='/learn' onMouseEnter={() => handleLinkHover(1)}>
+          <div>
+            <h5>Learn <span>-&gt;</span> </h5>
           </div>
         </Link>
-        <Link href="/about">
-          <div
-            onMouseEnter={() => handleLinkHover('/about')}
-            onMouseLeave={() => handleLinkHover('')}
-            className={`transition-transform duration-300 ${
-              hoveredLink === '/about' ? 'translate-x-1' : 'translate-x-0'
-            }`}
-          >
-            <h5>
-              About <span>-&gt;</span>
-            </h5>
+        <Link href='/about' onMouseEnter={() => handleLinkHover(2)}>
+          <div>
+            <h5>About <span>-&gt;</span> </h5>
           </div>
         </Link>
-        <Link href="/contact">
-          <div
-            onMouseEnter={() => handleLinkHover('/contact')}
-            onMouseLeave={() => handleLinkHover('')}
-            className={`transition-transform duration-300 ${
-              hoveredLink === '/contact' ? 'translate-x-1' : 'translate-x-0'
-            }`}
-          >
-            <h5>
-              Contact <span>-&gt;</span>
-            </h5>
+        <Link href='/contact' onMouseEnter={() => handleLinkHover(3)}>
+          <div>
+            <h5>Contact <span>-&gt;</span> </h5>
           </div>
         </Link>
 
         <SocialLinks />
       </nav>
+      <div className='circle' style={{ transform: `translateX(${circlePosition * 100}px)` }}>
+      <Image src="/logo.svg" 
+                                alt="calculated customs logo" 
+                                width={25} 
+                                height={25} 
+                                priority />
+        </div>
     </>
   );
 };
 
-export default NavBar;
+export default Navbar;
 
