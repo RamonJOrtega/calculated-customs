a22c739fc4435940acf3da9d68c3b216b3db70e3
RamonJOrtega <ramon.justis.ortega@gmail.com>
2023-07-02 09:22:50 -0700

    change transition of logo from .3 to .4s in navbar.tsx component and change original position of logo in globals.css to top 5% and left 20%

--- app/z components/NavBar.tsx
+++ app/z components/NavBar.tsx
@@ -15,7 +15,7 @@ import { SetStateAction, useState } from 'react';
 //                 <Link href="/" >
 //                     <div >
 //                         <h3 className='flex items-center'>      
-//                         CALCULATED CUSTOMS <span className="ml-2"> 
+//                         CALCULATED CUSTOMS <span className="mr-2"> 
 //                                 <Image src="/logo.svg" 
 //                                 alt="calculated customs logo" 
 //                                 width={25} 
@@ -67,19 +67,13 @@ const Navbar = () => {
           </div>
         </Link>
         <Link href='/learn' onMouseEnter={() => handleLinkHover(2)}>
-          <div>
-            <h5>Learn </h5>
-          </div>
+            <h5 className='mr-6'>Learn </h5>
         </Link>
         <Link href='/about' onMouseEnter={() => handleLinkHover(3)}>
-          <div>
-            <h5>About </h5>
-          </div>
+            <h5 className='mr-6'>About </h5>
         </Link>
         <Link href='/contact' onMouseEnter={() => handleLinkHover(4)}>
-          <div>
-            <h5>Contact </h5>
-          </div>
+            <h5 className='mr-6'>Contact </h5>
         </Link>
 
         <SocialLinks />
