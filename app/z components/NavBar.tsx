d12580572516f9d81ee250d4e89b89bbfa22252f
RamonJOrtega <ramon.justis.ortega@gmail.com>
2023-07-02 17:34:52 -0700

    modify navbar.tsx to add social links as 5th index

--- app/z components/NavBar.tsx
+++ app/z components/NavBar.tsx
@@ -7,64 +7,29 @@ import { AiOutlineInstagram, AiOutlineYoutube } from 'react-icons/ai';
 import { SetStateAction, useState } from 'react';
 
 
-// export default function NavBar() {
-    
-//     return(
-//         <>
-//             <nav className='flex justify-between flex-wrap mb-8'>
-//                 <Link href="/" >
-//                     <div >
-//                         <h3 className='flex items-center'>      
-//                         CALCULATED CUSTOMS <span className="mr-2"> 
-//                                 <Image src="/logo.svg" 
-//                                 alt="calculated customs logo" 
-//                                 width={25} 
-//                                 height={25} 
-//                                 priority />
-//                             </span> 
-//                         </h3>
-//                     </div>
-//                 </Link>
-//                 <Link  href="/learn">
-//                     <div >
-//                         <h5 >Learn </h5>
-//                     </div>
-//                 </Link>
-//                 <Link  href="/about">
-//                     <div >
-//                         <h5 >About </h5>
-//                     </div>
-//                 </Link>
-//                 <Link  href="/contact">
-//                     <div >
-//                         <h5 >Contact </h5>
-//                     </div>
-//                 </Link>
-          
-//                 <SocialLinks />
-//             </nav>
-//         </>
-//     )
-// }
-
-
-
 const Navbar = () => {
   const [circlePosition, setCirclePosition] = useState(0);
 
-  const handleLinkHover = (index) => {
+  const handleLinkHover = (index: SetStateAction<number>) => {
     setCirclePosition(index);
   };
 
   return (
     <>
+
       <nav className='flex justify-between flex-wrap mb-8 '>
+     
         <Link href='/' onMouseEnter={() => handleLinkHover(1)}>
-          <div className='mr-6'>
-            <h3 className='flex items-center'>
+          <div className='mr-6 flex items-center'>
+            <h3>
               CALCULATED CUSTOMS
             </h3>
-          </div>
+            {/* <div className='circle' style={{ transform: `translateX(${circlePosition * 100}%)` }}> */}
+            <div className='circle' style={{ transform: `translateX(${circlePosition * 100}%)` }}>
+
+              <Image src="/logo.svg" alt="calculated customs logo" width={25} height={25} priority />
+            </div>
+        </div>
         </Link>
         <Link href='/learn' onMouseEnter={() => handleLinkHover(2)}>
             <h5 className='mr-6'>Learn </h5>
@@ -72,15 +37,16 @@ const Navbar = () => {
         <Link href='/about' onMouseEnter={() => handleLinkHover(3)}>
             <h5 className='mr-6'>About </h5>
         </Link>
-        <Link href='/contact' onMouseEnter={() => handleLinkHover(4)}>
+        <Link href='/contact'  onMouseEnter={() => handleLinkHover(4)}>
             <h5 className='mr-6'>Contact </h5>
         </Link>
 
+        <div onMouseEnter={() => handleLinkHover(5)}>
         <SocialLinks />
+
+        </div>
       </nav>
-      <div className='circle' style={{ transform: `translateX(${circlePosition * 100}px)` }}>
-        <Image src="/logo.svg" alt="calculated customs logo" width={25}  height={25} priority />
-      </div>
+
     </>
   );
 };
