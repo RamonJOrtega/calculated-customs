3dd951f20f0b77986e5dea6ac4152dd7e80f89b5
RamonJOrtega <ramon.justis.ortega@gmail.com>
2023-07-09 10:53:19 -0700

    move tireWidth data list form CalcInput.tsx component to its own WidthDataList.tsx component

---
+++ app/z components/WidthDataList.tsx
@@ -0,0 +1,19 @@
+
+interface WidthDataList {
+    title: String
+}
+
+export default function WidthDataList(props: any) {
+    return (
+      <datalist id={props.title} >
+        <option value="105"></option><option value="115"></option><option value="125"></option><option value="135"></option>
+        <option value="145"></option><option value="155"></option><option value="165"></option><option value="175"></option>
+        <option value="185"></option><option value="195"></option><option value="205"></option><option value="215"></option>
+        <option value="225"></option><option value="235"></option><option value="245"></option><option value="255"></option>
+        <option value="265"></option><option value="275"></option><option value="285"></option><option value="295"></option>
+        <option value="305"></option><option value="315"></option><option value="325"></option><option value="335"></option>
+        <option value="345"></option><option value="355"></option><option value="375"></option><option value="395"></option>
+        <option value="405"></option>
+    </datalist>
+  )}
+  
