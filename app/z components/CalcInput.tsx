69573d56fe9fb4550b3bd287aa13837faa85045f
RamonJOrtega <ramon.justis.ortega@gmail.com>
2023-07-12 14:24:03 -0700

    change calculator input font to bold in CalcInput.tsx

--- app/z components/CalcInput.tsx
+++ app/z components/CalcInput.tsx
@@ -50,9 +50,9 @@ const CalcInput: React.FC<CalcInputProps> = (props) => {
         );
     } else if (typeof props.value === 'string') {
         if (props.title.includes("Inertia")) {  //results elements are special inputs
-            inputElement = (<input className='w-full rounded-md bg-neutral-900 text-yellow-200' type="number" value={props.value} disabled />);
+            inputElement = (<input className='w-full rounded-md bg-neutral-900 text-yellow-200 font-bold' type="number" value={props.value} disabled />);
         } else {                                //all other elements are standard inputs
-            inputElement = (<input className='w-full rounded-md bg-neutral-800' type="number" value={props.value} ref={inputRef} onInput={handleInput} pattern="[0-9.]" placeholder={props.placeHolder} onClick={()=>{props.setValue("")}} list = {compactTitleId} max="500" min="0" step = "0.1"/>);
+            inputElement = (<input className='w-full rounded-md bg-neutral-800 font-bold' type="number" value={props.value} ref={inputRef} onInput={handleInput} pattern="[0-9.]" placeholder={props.placeHolder} onClick={()=>{props.setValue("")}} list = {compactTitleId} max="500" min="0" step = "0.1"/>);
         }
     }
     
