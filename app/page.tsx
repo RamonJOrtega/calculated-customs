ca0bdc55afba67f9e292c44a29f94d32c5e8c58c
RamonJOrtega <ramon.justis.ortega@gmail.com>
2023-08-29 12:50:26 -0700

    reformat main page.tsx to simplify UX and UI

--- app/page.tsx
+++ app/page.tsx
@@ -8,29 +8,30 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
 
   return (
-    <section className='flex flex-col items-center text-center'>
+    <section className=' text-left'>
         {/* <h1>KNOW WHAT TO EXPECT FROM YOUR WHEELS AND TIRES</h1> */}
         {/* <h1>COMPARE WHEEL AND TIRE SETS | KNOW YOU CHOSE SPEED </h1> */}
-        <h1> CALCULATE INERTIA | COMPARE WHEELS AND TIRES | CHOOSE THE FASTEST SET </h1>
+        <h1> CHOOSE THE FASTEST WHEEL-TIRE-SET BY INERTIA </h1>
         <br></br>
-        <p>
+        <p  >
           <span><a href="https://www.tkqlhce.com/click-100641204-10377976" className='animate-pulse hover:scale-110'><strong>Browse Wheels.</strong></a></span> &#160;   
           <span ><a href="https://www.kqzyfj.com/click-100641204-10377851"className='animate-pulse hover:scale-110'><strong>Browse Tires.</strong> </a></span>
+          Then, enter size and weight to calculate the fastest combo!
           <br></br><br></br>
-          Enter size and weight.
-          Calculate the fastest combo!
-          <br></br> <br></br>
-          Small, lightweight combinations have low rotational inertia numbers.
-          <br></br> <br></br>
-          The lower the number, the faster your zero-to-sixty time.
-        </p> 
+          </p> 
         <br></br>
-      <MainCalculator  />
+
+        <MainCalculator  />
 
         <br></br>
         <p >
-          Compare 2 wheel and tire combinations at once to quantify the zero-to-sixty time difference. <br></br> <br></br>
-          You will noticeably FEEL acceleration benefits by lowering Rotational Inertia (even by 0.1 kg&#x2022;m<sup>2</sup>). 
+        Small, lightweight combinations have low rotational inertia numbers.
+          <br></br><br></br>
+          The lower the number, the faster your zero-to-sixty time.
+          
+          <br></br> <br></br>
+          Compare 2 wheel and tire combinations at once to quantify the zero-to-sixty-mph time difference. <br></br> <br></br>
+          You will noticeably FEEL acceleration benefits by lowering Rotational Inertia (even by small amounts of 0.1 kg&#x2022;m<sup>2</sup>). 
         </p>
     </section>
   )
