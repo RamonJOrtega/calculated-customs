40e7f978d2e4bc7e2f91035124da815296f9d6d7
RamonJOrtega <ramon.justis.ortega@gmail.com>
2023-08-31 16:15:04 -0700

    simplify SocialLinks.tsx html

--- app/z components/SocialLinks.tsx
+++ app/z components/SocialLinks.tsx
@@ -1,25 +1,15 @@
-import { Inter } from 'next/font/google'
 import Link from  'next/link';
 import { AiOutlineInstagram, AiOutlineYoutube } from "react-icons/ai"
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function SocialLinks() {
     
     return(
         <div className='flex'>
-        
-            <Link href="https://youtube.com/channel/UCazq-Zh2hxOYou0geTXhOTA">
-                <div >
-                    <h3 >  <AiOutlineYoutube /> </h3>
-                </div>
+            <Link className='p-2' href="https://youtube.com/channel/UCazq-Zh2hxOYou0geTXhOTA">
+                <div > <h3 > <AiOutlineYoutube /> </h3></div>
             </Link>
-        
-        
-            <Link href="https://www.instagram.com/calculatedcustoms">
-                <div >
-                    <h3 >  <AiOutlineInstagram /> </h3>
-                </div>
+            <Link className='p-2' href="https://www.instagram.com/calculatedcustoms">
+                <div > <h3> <AiOutlineInstagram /> </h3></div>
             </Link>  
         </ div> 
     )
