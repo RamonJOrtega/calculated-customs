31fae2355b347966fd3ad02100ccf09cc5f136c6
RamonJOrtega <ramon.justis.ortega@gmail.com>
2023-09-03 17:47:27 -0700

    add email sign up button to footer.tsx

--- app/z components/Footer.tsx
+++ app/z components/Footer.tsx
@@ -4,11 +4,18 @@ import Link from  'next/link';
 export default function NavBar() {
     
     return(
-        <nav className='flex justify-evenly flex-wrap'>
-            
+        <nav className='flex justify-between flex-wrap'>
+            <Link href="/email-sign-up">
+            <h3 className="bg-gradient-to-r from-amber-50 to-yellow-200 text-neutral-900 text-sm font-bold px-2 py-1 m-1 rounded-md animate-pulse">
+                Email Sign-Up
+            </h3> 
+            </Link>
+
+
             <Link href="/terms-and-conditions">
                     <h5 >Terms and Conditions </h5>
             </Link>
+            
             <Link href="/privacy-policy">
                     <h5 >Privacy Policy  </h5>
             </Link>
