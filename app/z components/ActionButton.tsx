c0de8be4268a0ec513de2ae674e1785cbf3a692c
RamonJOrtega <ramon.justis.ortega@gmail.com>
2023-09-09 10:55:03 -0700

    create ActionButton.tsx component to pass in text and type :

---
+++ app/z components/ActionButton.tsx
@@ -0,0 +1,19 @@
+import { type } from "os"
+
+interface ButtonProps {
+  type: "button" | "submit" | "reset";
+  onClick?: () => void;
+  text: React.ReactNode;
+}
+
+const ActionButton: React.FC<ButtonProps> = ({ type, onClick, text }) => {
+  return (
+    <button type={type} onClick={onClick}
+    className="bg-gradient-to-r from-amber-50 to-yellow-200 text-neutral-900 
+                text-sm font-bold px-2 py-1 m-1 rounded-md animate-pulse">
+      {text}
+    </button>
+  );
+};
+
+export default ActionButton;
