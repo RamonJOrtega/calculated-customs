504e765a04ac647f8109c2c8f454e98459c98e8f
RamonJOrtega <ramon.justis.ortega@gmail.com>
2023-09-09 10:22:31 -0700

    add margin to email sign up page

--- app/email-sign-up/page.tsx
+++ app/email-sign-up/page.tsx
@@ -15,7 +15,7 @@ export default function emailSignUp() {
         return emailRegex.test(text)
     }
     
-    const create = async(emailText:string) => {
+    const registerEmail = async(emailText:string) => {
         // if text is valid, make apost request to pocketbase db, otherwise update state of input to display invalid message
         if (isEmailTextValid(emailText)) {
             await fetch('http://127.0.0.1:8090/api/collections/email_list/records', {
@@ -30,26 +30,19 @@ export default function emailSignUp() {
     return(
         <div>
             <section>
-                <div className='relative w-full h-96 overflow-hidden rounded-lg flex flex-col justify-center'>
-                    <Image 
-                        src={pagePicture}
-                        alt="front end of a dodge charger"
-                        placeholder="blur"
-                        //fill={true}
-                        />
+                <div className='relative w-full h-96 overflow-hidden rounded-lg flex flex-col justify-center mb-4'>
+                    <Image src={pagePicture} alt="front end of a dodge charger"placeholder="blur" />
                 </div> 
-                    <br></br>
                     <h1>NEW FEATURES ADDED A QUARTER MILE AT A TIME</h1>
                     <div className="flex flex-wrap gap-8 mt-8">
                     <div>
                         <h2>Never Miss a Thing!</h2>
                         <p>Enter your email address to get the latest news and gain exclusive access to features!</p>
                         <p>You'll be the first to know about new calculators and wheel and tire disounts. </p>
-                        <br></br>
                     </div>
                     <div>
                         <h2 >Our Promise</h2>
-                        <p>We send only impactful content.</p>
+                        <p>We will send only impactful content.</p>
                         <p>We will never spam email you.</p> 
                         <p>We will never share your data.</p>
                         <p>Unsubscribe anytime!</p>
@@ -62,7 +55,7 @@ export default function emailSignUp() {
                                     Subscribe
                             </button>
 
-                            <button type="submit" className="bg-gradient-to-r from-amber-50 to-yellow-200 text-neutral-900 text-sm font-bold px-2 py-1 m-1 rounded-md animate-pulse">
+                            <button type="submit"  className="bg-gradient-to-r from-amber-50 to-yellow-200 text-neutral-900 text-sm font-bold px-2 py-1 m-1 rounded-md animate-pulse">
                                     Unsubscribe
                             </button>
                         </div>
